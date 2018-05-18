d49b41f5f8b39f3e34d03ebec2eaf2d47b263c2f
DiegoT <tellodiego@yahoo.com>
2018-05-18 20:13:53 +0000

    Check for 400 status response was added.

--- public/assets/js/shorten-ajax.js
+++ public/assets/js/shorten-ajax.js
@@ -8,25 +8,27 @@ function shorten() {
     method: 'POST',
     body: JSON.stringify({longUrl: longUrl})
   }).then(function(response) {
-    if (response.ok && response.status == 200) {
+    if (response.ok) {
       return response.json();
-    } else {
-      document.getElementById('short-url').innerHTML = 'Error shortening: ' + response.json.error;    
     }
   }).then(function(json) {
-    let teenyUrl = window.location.origin + BASE_PATH + json.shortKey;
+    if(!json.error) {
+      let teenyUrl = window.location.origin + BASE_PATH + json.shortKey;
 
-    let linkLongUrl = '<div><a class="long-link" href= "' + json.longUrl + '">' + json.longUrl + '</a></div>';
-    let linkShortUrl = '<a class="short-link" href= "' + teenyUrl + '">' + teenyUrl + '</a>';
-    let inputShortUrl = '<input id="copy-input" value="' + teenyUrl + '" tabindex="-1" readonly/>';
-    let copyShortUrl = '<a href="#" class="btn-copy-url">Copiar</a>';
+      let linkLongUrl = '<div><a class="long-link" href= "' + json.longUrl + '">' + json.longUrl + '</a></div>';
+      let linkShortUrl = '<a class="short-link" href= "' + teenyUrl + '">' + teenyUrl + '</a>';
+      let inputShortUrl = '<input id="copy-input" value="' + teenyUrl + '" tabindex="-1" readonly/>';
+      let copyShortUrl = '<a href="#" class="btn-copy-url">Copiar</a>';
 
-    document.getElementById('short-url').innerHTML = linkLongUrl + '<div>' + linkShortUrl + inputShortUrl + copyShortUrl + '</div>';
+      document.getElementById('short-url').innerHTML = linkLongUrl + '<div>' + linkShortUrl + inputShortUrl + copyShortUrl + '</div>';
 
-    var btnCopy = document.getElementsByClassName('btn-copy-url');
+      var btnCopy = document.getElementsByClassName('btn-copy-url');
 
-    if(btnCopy.length > 0) {
-      btnCopy[0].addEventListener('click', copyToClipboard);
+      if(btnCopy.length > 0) {
+        btnCopy[0].addEventListener('click', copyToClipboard);
+      }
+    } else {
+      document.getElementById('short-url').innerHTML = 'Error shortening: ' + json.error;    
     }
 
   }).catch(function(error) {
